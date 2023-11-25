2d4d58d268e30dc870172f479eba0b3e3887b0ae
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2023-11-25 12:42:59 +0530

    A production grade approch to handle environment variables

---
+++ src/env-variable/env-variable.js
@@ -0,0 +1,9 @@
+const envData = {
+    appwriteURL : String(import.meta.env.VITE_APP_APPWRITE_URL),
+    appwriteProjectID : String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID : String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionID : String(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketID : String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+}
+
+export default envData;
