fafe922572eec58749fc0c803646aff0817039ce
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2023-11-27 00:47:37 +0530

    html-react-parsing error

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import appwriteService from "../appwrite/config";
 import { Button, Container } from "../components";
-import {parse} from "html-react-parser";
+import parse from "html-react-parser";
 import { useSelector } from "react-redux";
 
 export default function Post() {
